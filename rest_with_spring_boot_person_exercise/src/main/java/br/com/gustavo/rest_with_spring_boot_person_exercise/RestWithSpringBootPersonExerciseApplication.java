9fb265581c83b26e7fa3b036e467b43c96acc6c9
gustavogermano <prestongu92@gmail.com>
2025-11-08 15:35:48 -0300

    Initial project configuration

---
+++ rest_with_spring_boot_person_exercise/src/main/java/br/com/gustavo/rest_with_spring_boot_person_exercise/RestWithSpringBootPersonExerciseApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.rest_with_spring_boot_person_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootPersonExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootPersonExerciseApplication.class, args);
+	}
+
+}
---
+++ rest_with_spring_boot_person_exercise/src/test/java/br/com/gustavo/rest_with_spring_boot_person_exercise/RestWithSpringBootPersonExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.rest_with_spring_boot_person_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootPersonExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
