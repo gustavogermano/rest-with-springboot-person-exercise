e39264ef9318aa57bc8be64fb15f867cdd844472
gustavogermano <prestongu92@gmail.com>
2025-11-19 15:37:01 -0300

    Implemented Dozer mapper, unit tests

--- rest_with_spring_boot_person_exercise/src/main/java/br/com/gustavo/rest_with_spring_boot_person_exercise/services/PersonServices.java
+++ rest_with_spring_boot_person_exercise/src/main/java/br/com/gustavo/rest_with_spring_boot_person_exercise/services/PersonServices.java
@@ -3,18 +3,20 @@ package br.com.gustavo.rest_with_spring_boot_person_exercise.services;
 import br.com.gustavo.rest_with_spring_boot_person_exercise.exception.ResourceNotFoundException;
 import br.com.gustavo.rest_with_spring_boot_person_exercise.model.Person;
 import br.com.gustavo.rest_with_spring_boot_person_exercise.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
+
 
 @Service
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository repository;
